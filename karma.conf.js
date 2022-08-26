e3cdee621019282f9ac358f8854a7e65e06a45ed
Mohit Saini <mohitsaini1196@gmail.com>
2022-08-26 12:02:17 +0100

    static build

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular1'),
+      dir: require('path').join(__dirname, './coverage/angular_examples'),
       subdir: '.',
       reporters: [
         { type: 'html' },
